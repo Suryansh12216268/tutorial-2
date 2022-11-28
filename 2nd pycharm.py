e2276c06de3f8adc7d692c585b632ce22afaded9
Chirag Sharma <119149402+Chirag12205500@users.noreply.github.com>
2022-11-29 00:04:14 +0530

    Add files via upload

---
+++ 2nd pycharm.py
@@ -0,0 +1,60 @@
+
+# implemention  of operators
+number1 = 10 # first variable
+number2 = 20 # second variable
+sum = number1+number2 #It will add two variables and display the result
+print("Sum of two number is:",sum)
+
+subtraction = number1-number2
+print("subtraction of two numbers:",subtraction)
+multiplication = number1*number2
+print("multiplication of two numbers is:",multiplication)
+
+division = number1/number2
+print("division of two number is:",division)
+
+remainder = number1%number2
+print("remainder of two number is:",remainder)
+
+number1 = 40
+number2 = 50
+integerdivision = number1//number2
+print("integerdivision of two numbers is:", integerdivision)
+
+number1 = 2
+number2 = 3
+power = number1**number2 # exponential value
+print("power of number1 ^ number2 is:",power)
+
+
+#Wap to convert celsius to fahrenheit
+celsius = 40;
+f = 32 +( 9/5 * celsius)
+print("After conversion the result is:",f)
+Side = 4
+Area = Side * Side
+print("Area of square is:", Area)
+
+Side = 4
+Perimeter = 4 * Side
+print("Perimeter of the square is:", Perimeter)
+b = 10
+a = 3
+a = a+2
+print(a)
+a+=2
+print("value of a is:",a)
+a+=b
+print(a)
+print("hello")
+
+i = 10
+if(i == 10):
+   print("value of i is 10") #here the space before print is needed because of this " : "
+
+
+
+
+
+
+
